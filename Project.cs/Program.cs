73b56e2ebbf1d67e1a0946a772ea29a9f7ac85b0
SumitomShome <sumitomshome@gmail.com>
2020-10-01 20:18:05 +0530

    [Sumitom] Refactor: Commiting UC1-Initializing

---
+++ Project.cs/Program.cs
@@ -0,0 +1,15 @@
+using System;
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Problem!");
+            //Initializing
+            int currentPosition = 0;
+            Console.WriteLine("Starting position is : " + currentPosition);
+        }
+    }
+}
+
